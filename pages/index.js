a831c9b8e8b7629996c4a9eecb80fef445e5edc9
guomengtao <rinuo@qq.com>
2024-12-08 17:00:59 +0800

    Add pages directory and home page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to My Blog Generator!</h1>
+        <p>This is the homepage of your Next.js app.</p>
+      </div>
+    );
+  }
